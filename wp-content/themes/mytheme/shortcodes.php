17a935b707ae60e98b87060f5cddeabfe5547713
TheoNorris <63970056+TheoNorris@users.noreply.github.com>
2024-01-26 21:25:21 +0100

    styled block 5

--- wp-content/themes/mytheme/shortcodes.php
+++ wp-content/themes/mytheme/shortcodes.php
@@ -18,4 +18,20 @@ function mytheme_shortcode_sample_project(){
 
 add_shortcode("sampleproject", "mytheme_shortcode_sample_project");
 
+function mytheme_shortcode_form(){
+    $attr = "form";
+
+    return '<form class="contact-form">
+    <input type="text" id="name" name="name" placeholder="Name"><br>
+    <input type="text" id="phone-number" name="number" placeholder="Phone Number" required><br>
+    <input type="text" id="email" name="email" placeholder="Email" required><br>
+    <input type="text" id="interest" name="interest" placeholder="Interested In"><br>
+    <textarea id="message" name="message" placeholder="Message" required></textarea>
+    <button class="contact-button" type="submit">SEND EMAIL <img src="' . get_template_directory_uri() . '/assets/images/arrow-right.svg" alt="arrow-right" /></button>
+</form>';
+
+}
+
+add_shortcode("form", "mytheme_shortcode_form");
+
 ?>
